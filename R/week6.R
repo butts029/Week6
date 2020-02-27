0741c4d0370b1ef5b9d52d1bba3dffeafa65035e
Jessica Butts <butts029@umn.edu>
2020-02-26 18:39:52 -0600

    Changed year to numeric type; Updated code for page start, title, and first author.

--- R/week6.R
+++ R/week6.R
@@ -11,12 +11,12 @@ length(citations) - length(citations_txt)
 # Data Cleaning
 library(tidyverse)
 library(rebus)
-library()
+
 sample(citations_txt, 10)
 citations_tbl <- enframe(citations_txt, name = "line", value = "cite") %>% 
   mutate(cite = str_remove_all(cite, or("\"", "'"))) %>%
-  mutate(year = str_extract(cite, pattern = "(" %R% repeated(DGT, 4, 4) %R% ")")) %>%
-  mutate(page_start = as.numeric(str_match(cite, pattern = capture(repeated(DGT, 1, 4)) %R% "-" %R% repeated(DGT, 1, 3))[,2])) %>%
+  mutate(year = as.numeric(str_extract(cite, pattern = repeated(DGT, 4, 4)))) %>%
+  mutate(page_start = as.numeric(str_match(cite, pattern = capture(repeated(DGT, 1, 4)) %R% or("-",SPC,"\\?") %R% repeated(DGT, 1, 4))[,2])) %>%
   mutate(perf_ref = str_detect(cite, fixed("performance", ignore_case = TRUE))) %>%
-  mutate(title = str_match(cite,  "\\(" %R% repeated(DGT, 4, 4) %R% "\\)" %R% DOT %R% SPC %R% capture(one_or_more(or(ALPHA, SPC, ",", ":","\\?"))) %R% DOT)[,2]) %>%
-  mutate(first_author = str_extract(cite, repeated(ALPHA, 1, Inf) %R% optional(or(",", DOT)) %R% optional(SPC) %R% one_or_more(ALPHA) %R% or(DOT, ",") %R% optional(SPC) %R% optional(ALPHA %R% DOT) %R% optional(",")))
+  mutate(title = str_match(cite, optional("\\(") %R% repeated(DGT, 4, 4) %R% optional(ALPHA) %R% optional("\\)") %R% optional(or(DOT, ",")) %R% SPC %R% capture(one_or_more(or(ALPHA, SPC, "-", ",",":","\\?"))) %R% optional(or(DOT, "\\?", ",")))[,2]) %>%
+  mutate(first_author = str_extract(cite, repeated(or("-",ALPHA), 1, Inf) %R% optional(or(",", DOT)) %R% optional(SPC) %R% one_or_more(ALPHA) %R% or(DOT, ",") %R% optional(SPC) %R% optional(ALPHA %R% DOT) %R% optional(",")))
